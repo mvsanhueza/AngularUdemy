649500c13802ba965713a6a06450d9eadd0e8a2e
Matias Sanhueza <msanhueza@btd.cl>
2024-01-29 17:33:16 -0300

    intro typescript hasta 24

---
+++ 01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -0,0 +1,49 @@
+
+interface Product {
+    description: string;
+    price: number;
+}
+
+const phone: Product = {
+    description: 'Nokia A1',
+    price: 150.0
+}
+
+const tablet: Product = {
+    description: 'iPad Air',
+    price: 250.0
+}
+
+interface TaxCalculaitonOptions{
+    tax: number;
+    products: Product[];
+
+}
+
+function taxCalculation(options: TaxCalculaitonOptions): [number, number] {
+
+    const {tax, products} = options;
+    let total = 0;
+
+    products.forEach(({price}) => {
+        total += price;
+    });
+
+    return [total, total * tax]
+
+}
+
+const shoppingCart = [phone, tablet];
+const tax = 0.15;
+
+const [total, taxTotal] = taxCalculation({
+    products: shoppingCart,
+    tax,
+})
+
+console.log('Total', total);
+console.log('Tax', taxTotal);
+
+
+
+export { }
