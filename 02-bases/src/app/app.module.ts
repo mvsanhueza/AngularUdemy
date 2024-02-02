45026f9498649206d83cd57a96923a325acb742f
Matias Sanhueza <msanhueza@btd.cl>
2024-02-02 18:22:10 -0300

    seccion 5, hasta ngClass

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/components/counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
+
 
 
 @NgModule({
@@ -22,6 +24,7 @@ import { ListComponent } from './heroes/list/list.component';
     AppRoutingModule,
     CounterModule,
     HeroesModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
