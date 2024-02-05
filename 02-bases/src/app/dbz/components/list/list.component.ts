5ce4266c653cf7c8ede1f03baf7e66850af1cbd3
Matias Sanhueza <msanhueza@btd.cl>
2024-02-05 17:32:50 -0300

    hasta clase 72

--- 02-bases/src/app/dbz/components/list/list.component.ts
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -12,5 +12,12 @@ export class ListComponent {
   public characterList: Character[] = [{
     name: 'Trunk',
     power: 10,
-  }]
+  }];
+
+  @Output()
+  public onDelete: EventEmitter<number> = new EventEmitter()
+
+  onDeleteCharacter(index: number): void {
+    this.onDelete.emit(index);
+  }
 }
