e3880762f862f21431eaa8a3494d5fa5b6e0b7f5
Matias Sanhueza <msanhueza@btd.cl>
2024-02-06 19:49:55 -0300

    termino seccion 5

--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -9,8 +9,18 @@ import { DbzService } from '../services/dbz.service';
 
 export class MainPageComponent {
 
-  constructor(public dbzService: DbzService){
+  constructor(private dbzService: DbzService){ }
 
+  get characters(): Character[]{
+    return [...this.dbzService.characters];
+  }
+
+  onDeleteCharacter(id:string):void{
+    this.dbzService.deleteCharacterById(id);
+  }
+
+  onNewCharacter(character:Character){
+    this.dbzService.addCharacter(character);
   }
 
   // public characters: Character[] = [{
