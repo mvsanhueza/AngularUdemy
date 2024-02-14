d4d39e7d3e32dccb2c490a46fb6ab71a8231009d
Matias Sanhueza <msanhueza@btd.cl>
2024-02-14 14:33:48 -0300

    termino seccion 7

--- 03-gifs-app/src/app/shared/shared.module.ts
+++ 03-gifs-app/src/app/shared/shared.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 
 
 
 @NgModule({
   exports: [
     SidebarComponent,
+    LazyImageComponent,
   ],
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    LazyImageComponent
   ],
   imports: [
     CommonModule
