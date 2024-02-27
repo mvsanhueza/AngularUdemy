e09092fbc4b973044501b13b7e22f10c301a2e26
Matias Sanhueza <msanhueza@btd.cl>
2024-02-27 17:58:21 -0300

    hasta clase 263

---
+++ 08-selectores/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+  {
+    path: '',
+    loadChildren: () => import('./countries/countries.module').then(m=>m.CountriesModule)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './selector-page.component.html',
+  styles: [
+  ]
+})
+export class SelectorPageComponent {
+
+}
