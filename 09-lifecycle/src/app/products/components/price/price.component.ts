7721725603970ac6bd15822897033c1c5eb0554c
Matias Sanhueza <msanhueza@btd.cl>
2024-04-15 18:51:13 -0400

    mapsApp

---
+++ 09-lifecycle/src/app/products/components/price/price.component.ts
@@ -0,0 +1,32 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+
+@Component({
+  selector: 'products-price',
+  templateUrl: './price.component.html',
+  styles: [
+  ]
+})
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+
+  @Input()
+  public price: number = 0
+
+  public interval$?: Subscription;
+
+  ngOnInit(): void {
+    console.log('Componente hijo: ngOnInit');
+    this.interval$ = interval(1000).subscribe(value => console.log(`Tick: ${value}`)
+    )
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Componente hijo: ngOnChanges');
+    console.log({changes});
+
+  }
+  ngOnDestroy(): void {
+    console.log('Componente hijo: ngOnDestroy');
+    this.interval$?.unsubscribe();
+
+  }
+}
---
+++ 10-mapsApp/src/app/maps/layout/maps-layout/maps-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './maps-layout.component.html',
+  styleUrls: [
+    './maps-layout.component.css'
+  ]
+})
+export class MapsLayoutComponent {
+
+}
